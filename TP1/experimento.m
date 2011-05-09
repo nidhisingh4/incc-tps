ae9f5e3336752f4cfdbd069b72d690498a01af43
miguelmsoler <miguelmsoler@15896790-2cd7-26f9-cf53-264f017f83cb>
2011-05-09 22:36:03 +0000

    corrección del fondo de las imágenes con máscara. Sigue fallando cada tanto pero funciona mejor

--- TP1/experimento.m
+++ TP1/experimento.m
@@ -1,12 +1,12 @@
 idSujeto=input('\nPor favor, ingrese su ID y presione Enter \n','s');
 
 window = Screen(0, 'OpenWindow');
-%letras=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','√±','o','p','q','r','s','t','u','v','w','x','y','z'];
-letras=['a';'b';'c';'d';'e';'f';'g';'h';'i';'j';'k';'l';'m';'n';'√±';'o';'p';'q';'r';'s';'t';'u';'v';'w';'x';'y';'z']; %PONER √±!!!
+%letras=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','Ò','o','p','q','r','s','t','u','v','w','x','y','z'];
+letras=['a';'b';'c';'d';'e';'f';'g';'h';'i';'j';'k';'l';'m';'n';'Ò';'o';'p';'q';'r';'s';'t';'u';'v';'w';'x';'y';'z']; %PONER Ò!!!
 mayuscula=1;
 minuscula=0;
-maxBlocks=1;		% Se har√°n 100 bloques
-estimPorBlock=10; 	% 270 est√≠mulos random por bloque
+maxBlocks=1;		% Se har·n 100 bloques
+estimPorBlock=10; 	% 270 estÌmulos random por bloque
 %burbujas=zeros(27,3,2);
 
 mbienvenida = imread('./bienvenida.png');
@@ -34,7 +34,7 @@ for b=1:maxBlocks
 	    WaitSecs(0.5);
 	    Screen('Close', textura);
 	    letra=round(rand()*26+1); 		%Devuelve un id de letra con una letra entre a-z ([1..27])
-	    tipografia=round(rand()*2+1);	%Devuelve el id de una tipograf√≠a de las utilizadas ([1..4])
+	    tipografia=round(rand()*2+1);	%Devuelve el id de una tipografÌa de las utilizadas ([1..4])
 	    
 	    if mod(e,estimPorBlock)>(estimPorBlock/2)
 		mayuscula=esMayuscula;
@@ -48,12 +48,12 @@ for b=1:maxBlocks
 		mayStr='min';
         end
         
-        [m1,m2,m3,m4,m5]=generarMascaras(burbujas);	%Devuelve un vector de 5 m√°scaras con la cantidad de burbujas especificada, y de acuerdo a las bandas predefinidas
+        [m1,m2,m3,m4,m5]=generarMascaras(burbujas);	%Devuelve un vector de 5 m·scaras con la cantidad de burbujas especificada, y de acuerdo a las bandas predefinidas
         nombreArchivo=[deblank(letras(letra,:)),'_',int2str(tipografia),'_',mayStr,'_0.pgm'];
 	    [f1,f2,f3,f4,f5,f6] = generarFiltros(strcat('./estimulos/',nombreArchivo));
-	    mletra1  = generarEstimulo(m1,m2,m3,m4,m5,f1,f2,f3,f4,f5,f6); %Devuelve una matriz con la imagen generada de la letra en may/min para la tipografia especificada, utilizando las m√°scaras indicadas
+	    mletra1  = generarEstimulo(m1,m2,m3,m4,m5,f1,f2,f3,f4,f5,f6); %Devuelve una matriz con la imagen generada de la letra en may/min para la tipografia especificada, utilizando las m·scaras indicadas
         
-	    mletra  = imread('/tmp/letra.pgm');
+	    mletra  = imread('c:\temp\letra.pgm');
 	    textura = Screen('MakeTexture', window, mletra);
 	    Screen('DrawTexture', window, textura);    
 	    Screen('Flip',window);	    
@@ -68,11 +68,11 @@ for b=1:maxBlocks
 	    [secs,tecla,deltasecs] = KbPressWait();
 	    teclaNombre = KbName(tecla);
 	    e=e+1;
-        % GUARDAR los valores obtenidos (m√°scaras, letra del est√≠mulo, letra predicha, tiempo transcurrido en responder)
+        % GUARDAR los valores obtenidos (m·scaras, letra del estÌmulo, letra predicha, tiempo transcurrido en responder)
 	    if deblank(letras(letra,:)) == teclaNombre
 		aciertos=aciertos+1;
 	    end
-	    if aciertos/e < 0.52 % Si la el porcentaje de aciertos es menor a 52%, se agrega una burbuja para el pr√≥ximo
+	    if aciertos/e < 0.52 % Si la el porcentaje de aciertos es menor a 52%, se agrega una burbuja para el prÛximo
 		burbujas=burbujas+1;
 	    end
 	end
