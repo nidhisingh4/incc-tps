af572a5c3879d77a187369e922be65c2ad672601
cgcossio@gmail.com <cgcossio@gmail.com@15896790-2cd7-26f9-cf53-264f017f83cb>
2011-05-14 00:16:48 +0000

    Se eliminó archivos innecesarios

--- trunk/TP1/prueba2.m
+++
@@ -1,7 +0,0 @@
-window = Screen(0, 'OpenWindow');
-mimagen = imread('./R_3_may_2.pgm');
-textura=Screen('MakeTexture', window, mimagen);
-Screen('DrawTexture', window, textura);
-Screen('Flip',window);
-WaitSecs(2);
-Screen('CloseAll');
